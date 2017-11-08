15c7334349ab170e9998c573a21cf86a1307259e
Dwi Ardi Irawan <dwiardiirawan@gmail.com>
2017-11-09 00:32:18 +0700

    hello world

--- src/main/java/com/dai/spring5/controller/GreetingController.java
+++ src/main/java/com/dai/spring5/controller/GreetingController.java
@@ -1,5 +1,18 @@
 package com.dai.spring5.controller;
 
-public class GreetingController {
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dai.spring5.model.Greeting;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Greeting greeting(@RequestParam(value="name", defaultValue="world") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
---
+++ src/main/java/com/dai/spring5/model/Greeting.java
@@ -0,0 +1,26 @@
+package com.dai.spring5.model;
+
+public class Greeting {
+	private Long id;
+	private String content;
+	
+	public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
